03f155d5b3089d5696527c255414c3ff37fa5066
priya-777 <priya7july2000@gmail.com>
2021-01-07 18:26:05 +0530

    Task-1 completed

--- src/main/java/com/example/demo/controller/DisplayHello.java
+++ src/main/java/com/example/demo/controller/DisplayHello.java
@@ -2,20 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Member;
-
 @RestController
 public class DisplayHello {
 
-//    @RequestMapping(method = RequestMethod.GET)
-//    public String homeInit() {
-//        return "home";
-//    }
-    @PostMapping("/")
-    public String homeInitpost(@RequestBody String msg) {
-        return "home";
+    @GetMapping("/")
+    public String homeInit() {
+        return "Hello";
     }
+
 }
