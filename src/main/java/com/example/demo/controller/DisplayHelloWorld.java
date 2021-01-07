9d53af539f7e61f54c8b9634be26ee1408ff095d
priya-777 <priya7july2000@gmail.com>
2021-01-07 18:46:32 +0530

    Task-1 Completed

---
+++ src/main/java/com/example/demo/controller/DisplayHelloWorld.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Member;
+@RestController
+public class DisplayHelloWorld {
+
+    @GetMapping("/")
+    public String displayHelloWorld() {
+        return "Hello World";
+    }
+
+}
